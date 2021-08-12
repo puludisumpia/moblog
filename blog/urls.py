8812f92b8724a5fc8e53366d5f2c72240e192886
Mpia <puludisumpia@gmail.com>
2021-08-12 13:34:24 +0200

    Ajout url

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("")
+    path("login/", views.connexion, name="connexion"),
+    path("register/", views.inscription, name="inscription"),
+    path("logout/", views.deconnexion, name="deconnexion"),
+    path("", views.index, name="index"),
+    path("blog/", views.post_list, name="post_list"),
+    path("blog/<slug:slug>/", views.post_detail, name="post_detail"),
+    path("apropos/", views.apropos, name="apropos"),
+    path("contact/", views.contact, name="contact"),
 ]
