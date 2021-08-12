351aaa5ed62deefd5b8fdf007564c4d613550523
Mpia <puludisumpia@gmail.com>
2021-08-12 23:21:50 +0200

    Conf

--- main/settings.py
+++ main/settings.py
@@ -175,5 +175,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "puludisumpia@gmail.com"
-EMAIL_HOST_PASSWORD = "251095,;Mpia"
+EMAIL_HOST_USER = "adresse_email@gmail.com"
+EMAIL_HOST_PASSWORD = "mit_de_passe"
